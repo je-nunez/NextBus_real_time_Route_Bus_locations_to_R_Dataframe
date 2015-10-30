e0a23bba42aaa5fbb3addd9e4eb9cf1c4c6d3f62
Jose E. Nunez <je.nunez@yahoo.ca>
2015-10-29 22:29:03 -0400

    1st draft of NextBus real-time api bus locations to R dataframe

---
+++ DataFrame_NextBus_real_time_change_in_Bus_locations.R
@@ -0,0 +1,77 @@
+
+# documentation of the R package "sp" (geo-spatial):
+#     https://cran.r-project.org/web/packages/sp/sp.pdf
+
+required_packages <- c("XML", "sp")
+
+missing_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+if( 0 < length(missing_packages) ) {
+  install.packages(missing_packages, dep=TRUE)
+}
+
+require(XML)
+require(sp)
+
+# how long in time to analyze changes in the vehicle locations in NextBus:
+# in the most recent 15 minutes
+#
+time_window_bus_movement <- 15 * 60
+
+# whether to debug our use of the NextBus Feed webservices or not
+
+debug_nextbus <- FALSE
+
+# the list of transit agencies for NextBus can be taken here:
+#
+#     http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList
+#
+# url_agencies <- "http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList"
+#
+# NextBus "sf-muni" = San Francisco Municipal Transportation Agency
+
+agency_code <- "sf-muni"
+
+# the bus route code. For a given transit agency, it can be taken from here:
+#
+#   http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=<agency-code> 
+#
+# In this case, the NextBus code is the same as the bus#, ie., San Francisco Muni 38R bus
+
+bus_route <- "38R"
+
+# NextBus Vehicle locations NextBus API
+#
+#   http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=<agency_tag>&r=<route tag>&t=<epoch time in msec>
+#
+base_nextbus_vehicle_locations_fmt <- "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=%s&r=%s&t=%d000"
+
+# current time
+options(digits.secs=3)
+now_str <- Sys.time()
+now_epoch <- as.integer(unclass(now_str))
+
+# Get the changes in vehicle locations since the last 15 minutes
+url_nextbus_feed <- sprintf(base_nextbus_vehicle_locations_fmt, 
+                            agency_code, bus_route, 
+                            now_epoch - time_window_bus_movement)
+
+if (debug_nextbus == TRUE ) {
+  
+  # download NextBus to a temporary XML file and parse it
+  tempfile <- "/tmp/nextbus_vehicle_locations.xml"
+  # tempfile <- tempfile()
+
+  # quiet = FALSE: debug download operation of NextBus Vehicle locations
+  # (to use RCurl could be another possibility)
+  download.file(url_nextbus_feed, tempfile, 
+                method="internal", quiet=FALSE, mode="w")
+    
+  xml_recs <- xmlParse(tempfile)
+} else {
+  xml_recs <- xmlParse(url_nextbus_feed)
+}
+
+# recs <- xmlToList(xml_recs)
+
+df <- as.data.frame(t(xmlSApply(xml_recs["/body/vehicle"],xmlAttrs)),
+                    stringsAsFactors=FALSE)
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# NextBus_real_time_Route_Bus_locations_to_R_Dataframe
-Queries the NextBus API to get the real-time change in the bus locations of a route into an R dataframe
+# NextBus real-time Route bus locations into an R Dataframe
+
+Queries the `NextBus` API to get the real-time change in the bus locations of a transit service route into an `R` dataframe
+
+What is loaded into an R dataframe is, according to current NextBus API, is at least the following
+columns for *each* vehicle that is servicing a transit route in quasi real-time (see column below
+on `secsSinceReport` for brief delay in real-time update):
+
+     Vehicle-id: the unique ID of the transit vehicle (e.g., of the bus, streetcar, etc)
+
+     The Transit-Route code
+
+     The ID of the direction in which this vehicle is heading
+
+     Latitude and Longitude: Geospatial position of the vehicle
+
+     Speed of the Vehicle (in Kilometers/hour)
+
+     Number of seconds passed since position and speed were last reported
+     
+     NextBus can predict the future position of the vehicle: a boolean value
+
+# WIP
+
+This project is a *work in progress*. The implementation is *incomplete* and
+subject to change. The documentation can be inaccurate.
+
