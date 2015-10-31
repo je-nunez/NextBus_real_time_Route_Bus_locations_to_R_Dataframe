62dec00189885e88f4c9b3404ae549d27e63a2fb
Jose E. Nunez <je.nunez@yahoo.ca>
2015-10-30 22:05:25 -0400

    a gglot() of R dataframe obtained from the real-time NextBus VehicleLocations XML API

--- DataFrame_NextBus_real_time_change_in_Bus_locations.R
+++ DataFrame_NextBus_real_time_change_in_Bus_locations.R
@@ -2,7 +2,7 @@
 # documentation of the R package "sp" (geo-spatial):
 #     https://cran.r-project.org/web/packages/sp/sp.pdf
 
-required_packages <- c("XML", "sp")
+required_packages <- c("XML", "sp", "gglot2")
 
 missing_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
 if( 0 < length(missing_packages) ) {
@@ -11,6 +11,7 @@ if( 0 < length(missing_packages) ) {
 
 require(XML)
 require(sp)
+library(ggplot2)
 
 # how long in time to analyze changes in the vehicle locations in NextBus:
 # in the most recent 15 minutes
@@ -33,7 +34,7 @@ agency_code <- "sf-muni"
 
 # the bus route code. For a given transit agency, it can be taken from here:
 #
-#   http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=<agency-code> 
+#   http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=<agency-code>
 #
 # In this case, the NextBus code is the same as the bus#, ie., San Francisco Muni 38R bus
 
@@ -51,21 +52,21 @@ now_str <- Sys.time()
 now_epoch <- as.integer(unclass(now_str))
 
 # Get the changes in vehicle locations since the last 15 minutes
-url_nextbus_feed <- sprintf(base_nextbus_vehicle_locations_fmt, 
-                            agency_code, bus_route, 
+url_nextbus_feed <- sprintf(base_nextbus_vehicle_locations_fmt,
+                            agency_code, bus_route,
                             now_epoch - time_window_bus_movement)
 
 if (debug_nextbus == TRUE ) {
-  
+
   # download NextBus to a temporary XML file and parse it
   tempfile <- "/tmp/nextbus_vehicle_locations.xml"
   # tempfile <- tempfile()
 
   # quiet = FALSE: debug download operation of NextBus Vehicle locations
   # (to use RCurl could be another possibility)
-  download.file(url_nextbus_feed, tempfile, 
+  download.file(url_nextbus_feed, tempfile,
                 method="internal", quiet=FALSE, mode="w")
-    
+
   xml_recs <- xmlParse(tempfile)
 } else {
   xml_recs <- xmlParse(url_nextbus_feed)
@@ -75,3 +76,12 @@ if (debug_nextbus == TRUE ) {
 
 df <- as.data.frame(t(xmlSApply(xml_recs["/body/vehicle"],xmlAttrs)),
                     stringsAsFactors=FALSE)
+
+# This is an example ggplot with the delay in the real-time of the locations
+
+plot_title <- sprintf("Delays in the Real-Time Location of the Transit Vehicles servicing the route %s (Transit Agency %s)",
+                      bus_route, toupper(agency_code))
+
+ggplot(df, aes(lat, lon, colour=secsSinceReport)) +
+  theme_minimal() + geom_point() +
+  labs(x = "Latitude", y = "Longitude", title=plot_title)
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # NextBus real-time Route bus locations into an R Dataframe
 
-Queries the `NextBus` API to get the real-time change in the bus locations of a transit service route into an `R` dataframe
+Queries the `NextBus` XML Feed API to get the real-time change in the bus locations of a transit
+service route into an `R` dataframe
 
 What is loaded into an R dataframe is, according to current NextBus API, is at least the following
 columns for *each* vehicle that is servicing a transit route in quasi real-time (see column below
@@ -17,11 +18,36 @@ on `secsSinceReport` for brief delay in real-time update):
      Speed of the Vehicle (in Kilometers/hour)
 
      Number of seconds passed since position and speed were last reported
-     
+
      NextBus can predict the future position of the vehicle: a boolean value
+
+# Note
+
+NextBus has a request limit that only one `vehicleLocations` query is allowed every
+5 minutes per client, as to avoid DoS abuse of their API:
+
+    In order to prevent some users from being able to download so much data that
+    it would interfere with other users we have imposed restrictions on data usage.
+    These limitations could change at any time. They currently are:
+
+       Maximum timespan for "vehicleLocations" command: 5min
 
 # WIP
 
 This project is a *work in progress*. The implementation is *incomplete* and
 subject to change. The documentation can be inaccurate.
+
+# An example ggplot of the dataframe
+
+This is a simple `ggplot()` graph in `R` of the daframe obtained from the
+NextBus real-time XML Feed API, giving the delay in updating the real-time
+position of the transit vehicles according to their geographical location
+for route `38R` in the `San Francisco Municipal Transportation Agency` at
+one given moment:
+
+![Delays of Real-Time Location of Vehicles in a Route](/delays_in_real_time_location_of_transit_vehicles.png?raw=true "Delays of Real-Time Location of Vehicles in a Route")
+
+(The geographical locations of the transit buses don't need to be
+ncessarily the coordinates of the bus stops, for a bus may be in
+movement when its location was registered or updated in NextBus.)
 
